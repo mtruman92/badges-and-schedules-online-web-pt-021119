1580c99b1e9a930c40655f9dd9c3d150c9916a4a
Mickae Truman <mtruman@nyit.edu>
2019-02-19 00:38:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def badge_maker(name)
-puts "Hello, my name is #{name}."
+return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(speakers)
@@ -7,3 +7,4 @@ def batch_badge_creator(speakers)
     badge_maker(name)
   end
 end
+
