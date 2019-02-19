09d099fbfdce173afd90eab09d8ec9b9e998094c
Mickae Truman <mtruman@nyit.edu>
2019-02-19 00:50:34 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  name.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.collect.each_with_index do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers). each do |speakers|
+    puts speakers
+  end
+  assign_rooms(speakers).each do |speakers|
+    puts speakers
+  end
+end
